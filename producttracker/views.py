efd133bdfbe8fd07095c7ca1606aa55aa08c8f04
Zachary Levy <zacharylevy@Zacharys-MacBook-Pro.local>
2014-09-02 21:06:13 -0400

    added index

--- producttracker/views.py
+++ producttracker/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from main.models import *
+from main.utils import *
+import datetime
+import string
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Hello, world. You're at the poll index.")
+
+
+
+
+
+
+
+
+
