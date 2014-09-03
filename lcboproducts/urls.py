331d94424bfc492d515e094a2b99fc94225664a3
Zachary Levy <zacharylevy@Zacharys-MacBook-Pro.local>
2014-09-02 22:03:03 -0400

    index, added app urls

--- lcboproducts/urls.py
+++ lcboproducts/urls.py
@@ -5,8 +5,8 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'lcboproducts.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    # url(r'^$', 'lcboproducts.views.index', name='index'),
+    url(r'^$', include('producttracker.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
--- producttracker/views.py
+++ producttracker/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
-from django.core.urlresolvers import reverse
-from main.models import *
-from main.utils import *
 import datetime
 import string
 
