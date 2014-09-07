1785cf3deedc8fedec2434f578f42f3ef36c6131
Zachary Levy <zacharylevy@Zacharys-MacBook-Pro.local>
2014-09-07 12:12:33 -0400

    added admin interface for viewing data

--- producttracker/admin.py
+++ producttracker/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from producttracker.models import TrackProduct
+from producttracker.models import Product
+from producttracker.models import StoreInventory
+
+admin.site.register(TrackProduct)
+admin.site.register(Product)
+admin.site.register(StoreInventory)
