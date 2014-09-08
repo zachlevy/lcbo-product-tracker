34a4774a9c420d007e19a6d5396c52fde7e2e198
Zachary Levy <zacharylevy@Zacharys-MacBook-Pro.local>
2014-09-08 09:53:52 -0400

    post gres heroku

--- lcboproducts/settings.py
+++ lcboproducts/settings.py
@@ -78,7 +78,10 @@ USE_TZ = True
 # heroku
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+DATABASES = {
+    "default": dj_database_url.config(default='postgres://localhost'),
+}
+#DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
